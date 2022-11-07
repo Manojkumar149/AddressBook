c884bd4867b98ed4ab1abaee8549996a0c9638bb
Manoj Kumar Manukonda <manojkumarmanukonda77@gmail.com>
2022-11-07 13:09:02 +0530

    [Manoj] Add: create Contact

---
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class AddressBookMain
+    {
+        List<Contacts> address = new List<Contacts>();
+        public void CreateContact()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name :");
+            contacts.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            contacts.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            contacts.Address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            contacts.City = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            contacts.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            contacts.ZipCode = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            contacts.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            contacts.EmailId = Console.ReadLine();
+            address.Add(contacts);
+            Display();
+        }
+        public void Display()
+        {
+            foreach (var contacts in address) {
+                Console.WriteLine("\nContact Details " + "\n" + contacts.FirstName + "\n" + contacts.LastName + "\n" + contacts.Address + "\n" +
+                        contacts.City + "\n" + contacts.State + "\n" + contacts.ZipCode + "\n" + contacts.PhoneNumber + "\n" + contacts.EmailId);
+            }
+        }
+    }
+}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -5,19 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Problem");
-            Contacts contacts = new Contacts()
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                ZipCode = Convert.ToInt64(Console.ReadLine()),
-                PhoneNumber = Console.ReadLine(),
-                EmailId = Console.ReadLine()
-            };
-            Console.WriteLine("Contact Details " + "\n" + contacts.FirstName + "\n" + contacts.LastName + "\n" + contacts.Address + "\n" +
-                contacts.City + "\n" + contacts.State + "\n" + contacts.ZipCode + "\n" + contacts.PhoneNumber + "\n" + contacts.EmailId);
+            AddressBookMain addressBookMain = new AddressBookMain();
+            addressBookMain.CreateContact();   
         }
     }
 }
